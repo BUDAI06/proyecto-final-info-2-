f4bf7176a89122a3ff9bb2efde156834edd1f42e
davidloaiza2-Lucky <david.loaiza2@udea.edu.co>
2025-12-06 00:22:36 -0500

    Update __init__.py
    Convierte la carpeta en un paquete para que el controlador encuentre facilmente

---
+++ proyecto final/model/__init__.py
@@ -0,0 +1,7 @@
+
+from .autenticacion_model import AutenticacionModel
+from .procesamiento_imagenes_model import ProcesadorImagenesMedicasModelo
+from .procesamiento_senales_model import ProcesadorSenalesModelo
+from .procesamiento_tabular_model import ProcesadorTabularModelo
+
+from .base_datos_model import BaseDatosModel
